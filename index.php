0eaa011f49c8eba8056485a9c3df21c2bd7a5764
Sathinmanitha <sathinmanitha1909@gmail.com>
2024-05-15 10:03:49 +0530

    Set Upper Icon

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+  <!--Upper Icon-->
+  <link rel="shortcut icon" type="dp" href="./pics/graduate.png">
+
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
