53448b075fd1ce8025da66c9682b3a516f21667d
Nusrath cassim <154523455+NusrathCassim@users.noreply.github.com>
2024-05-22 12:54:00 +0530

    modify-the-student-profile (#31)
    * modify-the-student-profile
    
    * Update connection.php
    
    Change the port number to default port
    
    ---------
    
    Co-authored-by: Ramitha Heshan <142712365+RamithaHeshan33@users.noreply.github.com>

--- view_profile/view_profile.php
+++ view_profile/view_profile.php
@@ -22,46 +22,92 @@ include_once('../assests/content/static/template.php');
     <link rel="stylesheet" href="style-view_profile.css">
 </head>
 <body>
-    <div class="container">
+<div class="container">
         <!-- //adding user profile image -->
-<div class="profilepic">
-    <a href="..\view_profile\Images\User Icon.png">
-        <img src="..\view_profile\Images\User Icon.png" alt="User Profile Icon">
-    </a>
-</div>
+    <div class="side-container">
+        <div class="profilepic">
+            <a href="..\view_profile\Images\graduate.png">
+                <img src="..\view_profile\Images\graduate.png" alt="User Profile Icon">
+            </a>
+        </div>
+        <div class="uni-pic-container">
+            <div class = "box">
+            <img src="..\view_profile\Images\uni-1.png" alt="uni-1-pic">
+            </div>
+            <div class = "box">
+            <img src="..\view_profile\Images\uni-2.png" alt="uni-1-pic">
+            </div> 
+        </div>
+    </div>
+    
 
     <div class="profileTop">
 
         <b><h1 class="topname"> <span><?php echo isset($student_name) ? $student_name : ''; ?></span></h1></b>
         <h6><?php echo isset($course) ? $course : ''; ?></h6>
-        <br><br>
+        <br>
             
             <div class="p_data">
-                <h2 class="personal_details">Personal Details</h2><hr>
-                <h4 class="studentid">Student ID :  <span><?php echo isset($username) ? $username : ''; ?></span></h4>
+                <div class = "personal_details">
+                    <h2> Personal Details</h2>
+                    <hr>
+                    <br>
+                    <div class = "form-row">
+                        <span class="form-label">Student ID:</span>
+                        <span class="form-value"><?php echo isset($username) ? $username : ''; ?></span>
+                    </div>
+                    <div class = "form-row">
+                        <span class="form-label">Batch No:</span>
+                        <span class="form-value"><?php echo isset($batch_number) ? $batch_number : ''; ?></span>
+                    </div>
+                    <div class = "form-row">
+                        <span class="form-label">Gender:</span>
+                        <span class="form-value"><?php echo isset($gender) ? $gender : ''; ?></span>
+                    </div>
+                    <div class = "form-row">
+                        <span class="form-label">Date Of Birth:</span>
+                        <span class="form-value"><?php echo isset($dob) ? $dob : ''; ?></span>
+                    </div>
+                    <div class = "form-row">
+                        <span class="form-label"> National Identification No:</span>
+                        <span class="form-value"><?php echo isset($nic) ? $nic : ''; ?></span>
+                    </div>
+                    <div class = "form-row">
+                        <span class="form-label"> E-mail:</span>
+                        <span class="form-value"><?php echo isset($email) ? $email : ''; ?></span>
+                    </div>
+                    <div class = "form-row">
+                        <span class="form-label"> Contact info:</span>
+                        <span class="form-value"><?php echo isset($contact) ? $contact : ''; ?></span>
+                    </div>
+                </div>
+               
 
-                <h4 class="Batch_No"> Batch No :  <span><?php echo isset($batch_number) ? $batch_number   : ''; ?></span></h4>
-
-                <h4 class="Gender">   Gender :  <span><?php echo isset($gender) ? $gender : ''; ?></span></h4>
-
-                <h4 class="birth_date">Birth date :  <span><?php echo isset($dob) ? $dob : ''; ?></span></h4>
-
-                <h4 class="NIC">       National Identification No. :  <span><?php echo isset($nic) ? $nic : ''; ?></span></h4>
-
-                <h4 class="Email">     Email :  <span><?php echo isset($email) ? $email : ''; ?></span></h4>
-
-                <h4 class="Contact">   Contact :  <span><?php echo isset($contact) ? $contact : ''; ?></span></h4>
-                <br><br>
             </div>
-
+            <br><br>
             <div class="University_data">
-                <h2 class="uni_details">University Details</h2><hr>
-                <h4 class="uni">Awarding University :  <span><?php echo isset($awarding_uni) ? $awarding_uni : ''; ?></span></h4>
-                <h4 class="uni_no">University Number :  <span><?php echo isset($uni_number) ? $uni_number   : ''; ?></span></h4>
-                <h4 class="center">Local Education Center :  <span><?php echo isset($lec) ? $lec : ''; ?></span></h4>
+                <div class = "uni_details">
+                    <h2>University Details</h2>
+                    <hr>
+                    <br>
+                    <div class = "form-row">
+                        <span class="form-label">Awarding University:</span>
+                        <span class="form-value"><?php echo isset($awarding_uni) ? $awarding_uni : ''; ?></span>
+                    </div>
+                    <div class = "form-row">
+                        <span class="form-label">University Number:</span>
+                        <span class="form-value"><?php echo isset($uni_number) ? $uni_number : ''; ?></span>
+                    </div>
+                    <div class = "form-row">
+                        <span class="form-label">Local Education Center:</span>
+                        <span class="form-value"><?php echo isset($lec) ? $lec : ''; ?></span>
+                    </div>
+
+                </div>
+                
             </div>
     </div>
-    </div>
+</div>
 
     
 </body>
