34f4600d96d82a090bd89f76c98594ee38d32dee
Nusrath cassim <154523455+NusrathCassim@users.noreply.github.com>
2024-05-27 20:51:55 +0530

    modifying-view-payment-status-section (#50)
    * update connection.php
    
    * updating-make-payment-section
    
    modifying-make-payment-section
    
    * Update connection.php
    
    * modifying-uploading-payment-section
    
    modifying-uploading-payment-section
    
    * little-update-on-upload-payment-receipt-css
    
    * Update connection.php
    
    * Update connection.php
    
    * updating-payment-status-section
    
    ---------
    
    Co-authored-by: Ramitha Heshan <142712365+RamithaHeshan33@users.noreply.github.com>

--- PaymentSection/view_paymentStatus/view_paymentStatus.php
+++ PaymentSection/view_paymentStatus/view_paymentStatus.php
@@ -33,35 +33,41 @@ $result = $stmt->get_result();
 
 </head>
 <body>
-<div class="table">
-        <table>
-            <tr>
-                <th>No</th>
-                <th>Paid Date</th>
-                <th>Description</th>
-                <th>Amount</th>
-                <th>Payment Link</th>
-            </tr>
-            <?php
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    $amount = $row['amount'];
-                    $record_id = $row['no']; // Use record id to uniquely identify the record
-                    echo "<tr>";
-                    echo "<td>" . htmlspecialchars($row['no']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['paid_date']) . "</td>";
-                    echo "<td>" . htmlspecialchars($row['description']) . "</td>";
-                    echo "<td>" . htmlspecialchars($amount) . "</td>";
-                    echo '<td> <button type="submit" class="view-link">PRINT</button> </td>';
-                    echo "</tr>";
+    <div class="container">
+        <div class="table">
+            <table>
+                <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Paid Date</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Payment Link</th>
+                </tr>
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $amount = $row['amount'];
+                        $record_id = $row['no']; // Use record id to uniquely identify the record
+                        echo "<tr>";
+                        echo "<td  data-cell = 'No'>" . htmlspecialchars($row['no']) . "</td>";
+                        echo "<td  data-cell = 'Paid Date'>" . htmlspecialchars($row['paid_date']) . "</td>";
+                        echo "<td  data-cell = 'Description'>" . htmlspecialchars($row['description']) . "</td>";
+                        echo "<td  data-cell = 'Amount'>" . htmlspecialchars($amount) . "</td>";
+                        echo '<td  data-cell = "Payment Link"> <button type="submit" class="view-link">PRINT</button> </td>';
+                        echo "</tr>";
+                    }
+                } 
+                else {
+                    echo "<tr><td colspan='5'>No payments found</td></tr>";
                 }
-            } 
-            else {
-                echo "<tr><td colspan='5'>No payments found</td></tr>";
-            }
-            $conn->close();
-            ?>
-        </table>
+                $conn->close();
+                ?>
+                </thead>
+            </table>
+        </div>
+        
     </div>
+  
 </body>
 </html>
