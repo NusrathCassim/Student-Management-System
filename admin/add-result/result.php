bb68aaf78f50c31732183074ce4fedeed064767b
Sathin Manitha <140915682+Sathinmanitha@users.noreply.github.com>
2024-06-15 12:00:29 +0530

    Template Error in Admin- Add Result Page  (#134)
    * Developing the Admin Dashboard
    
    * Assignment Submission Page Error Fixing
    
    * Fixing Error #97
    
    * Fix the error in vacancy page
    
    * Template Error in Admin- Add Result Page #133

--- admin/add-result/result.php
+++ admin/add-result/result.php
@@ -15,6 +15,10 @@ include_once('../assests/content/static/template.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campus Student Management System - Select Course</title>
+    <!--Template Css-->
+    <link rel="stylesheet" href="../style-template.css">
+
+    <!--Result Css-->
     <link rel="stylesheet" href="result.css">
 </head>
 <body>
